eb45378c4cb8105bfaba4f4ec8583fc39c9ddf59
marychka2335 <marychka2335@icloud.com>
2023-12-17 22:46:37 +0200

    second task commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Player('vimeo-player');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
