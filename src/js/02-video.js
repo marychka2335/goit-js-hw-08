782ade1117b0612685f39ed9226ba16e012b586c
marychka2335 <marychka2335@icloud.com>
2023-12-18 22:52:02 +0200

    secondtask recover SetTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,10 @@ const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-
+const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+if (savedTime !== null) {
+  player.setCurrentTime(savedTime);
+}
 function onPlay(data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+    localStorage.setItem("videoplayer-current-time", JSON.stringify({seconds}));
 }
