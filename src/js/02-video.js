7bb1a054cfac4057a9b2e24d1b2ce61b7f214d41
marychka2335 <marychka2335@icloud.com>
2023-12-18 23:14:15 +0200

    second task commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,6 +14,6 @@ const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 if (savedTime !== null) {
   player.setCurrentTime(savedTime);
 }
-function onPlay(data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify({seconds}));
-}
+// function onPlay(data) {
+//     localStorage.setItem("videoplayer-current-time", JSON.stringify({seconds}));
+// }
